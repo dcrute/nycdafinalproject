9a051c906b79c1c9fc2a367fb9b358cec339bbb9
dcrute <dcrute@ecfs.org>
2014-07-18 18:39:33 -0400

    Family Site now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 User.create(email: "dcrute25@hotmail.com", lname: "cruté", fname: "darien")
 @id = User.last.id
 Profile.create(bday: "1988-02-25", username: "dcrute", password: "AdminPassword1", hometown: "bronx", user_id: @id, approved: true, admin: true)
+Post.create(user_id: @id, string_data: "Welcome to the Cruté Family site!")
+User.create(email: "chipper62@aol.com", lname: "westby", fname: "charles")
+@id = User.last.id
+Profile.create(bday: "1984-06-02", username: "cnotes", password: "Password1", hometown: "bronx", user_id: @id, approved: true, admin: false)
