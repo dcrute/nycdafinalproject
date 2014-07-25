5e9f5b2da7d31fdc1b744ddc4bcf68293b7661fc
dcrute <dcrute@ecfs.org>
2014-07-25 07:00:15 -0400

    readded S3

--- app.rb
+++ app.rb
@@ -8,16 +8,16 @@ require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 require 'fog'
 
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     :provider               => 'AWS',                        # required
-#     :aws_access_key_id      => ENV['S3_KEY'],                        # required
-#     :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                        # required
-#   }
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                        # required
+  }
 
-#   config.fog_directory  = "crutespeaks-assets"
-#   config.fog_public     = true
-# end
+  config.fog_directory  = "crutespeaks-assets"
+  config.fog_public     = true
+end
 
 use Rack::Flash, :sweep => true
 set :sessions, true
