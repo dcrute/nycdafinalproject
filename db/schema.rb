c2484d568428a8aa5d686600a6dcb48994095576
dcrute <dcrute@ecfs.org>
2014-08-14 11:29:42 -0400

    added links to feed and wall

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809195110) do
+ActiveRecord::Schema.define(version: 20140814014658) do
+
+  create_table "alerts", force: true do |t|
+    t.string  "alert"
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "comments", force: true do |t|
     t.string  "data"
